0a3706a219292346ae6d38cdab5fca903b816df8
devcrema <devcrema@gmail.com>
2019-05-25 12:32:42 +0900

    음식점 카테고리 구조 구현 중.

--- core/src/main/java/devcrema/spring_boot_toy/store/Category.java
+++ core/src/main/java/devcrema/spring_boot_toy/store/Category.java
@@ -1,28 +1,20 @@
 package devcrema.spring_boot_toy.store;
 
-import devcrema.spring_boot_toy.user.User;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Getter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Store {
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false)
     private String name;
-
-    @ManyToMany
-    private List<Category> categories;
-
-    @ManyToMany
-    private List<User> chefList;
 }
